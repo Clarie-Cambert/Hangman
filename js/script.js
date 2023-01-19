5ba581e0e112f710f9528876af1098b1354bcd60
Etudiant O'clock <clarie.cambert@gmail.com>
2023-01-19 20:04:17 +0100

    wip: mep de js (affichage du nombre de _ correspondant au nombre de lettre)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const wordEl = document.getElementsByClassName('text');
+const wordEl = document.getElementById('word');
 const wrongLetters = document.getElementById('wrong-letters');
 const btnReplay = document.getElementById('btn-message');
 const popup = document.getElementsByClassName('popup-content');
@@ -7,30 +7,31 @@ const notification = document.getElementById('notification');
 const bodyMan = document.querySelectorAll('.body-man');
 
 const words = [
-    'telephone', 'chaussures', 'cheveux', 'universel', 'cinema', 'sport', 'chat', 'chien', 'marvel', 'harry potter', 'voldemort', 'ordinateur', 'france', 'fenetre', 'tasse', 'constitution', 'code', 'bretagne', 'marseille', 'toilettes', 'chambre', 'restaurant', 'pomme', 'table', 'coussin', 'eponge', 'superman', 'paris', 'ampoule', 'croissant', 'pain au chocolat', 'oeufs', 'baguette', 'application', 'javascript'
+    'telephone', 'chaussures', 'cheveux', 'universel', 'cinema', 'sport', 'chat', 'chien', 'marvel', 'voldemort', 'ordinateur', 'france', 'fenetre', 'tasse', 'constitution', 'code', 'bretagne', 'marseille', 'toilettes', 'chambre', 'restaurant', 'pomme', 'table', 'coussin', 'eponge', 'superman', 'paris', 'ampoule', 'croissant', 'oeufs', 'baguette', 'application', 'javascript'
 ]
 
 let selectedWord = words[ Math.floor(Math.random() * words.length) ];
-
-const  goodLettersArray = [''];
+console.log(selectedWord);
+const  goodLettersArray = ['a', 'b', 'h', 'j'];
 const  wrongLettersArray = [''];
 
 // Afficher le mot caché, lettre par lettre
 function showWord() {
-    wordEl.innerHTML = `
+    
+    return wordEl.innerHTML = `
         ${selectedWord
             .split('')
             .map(
-                letter => `
-                    <span id="letter">
-                        ${goodLettersArray.includes(letter) ? letter : ''}
+                (letter) => `
+                    <span id="single-letter">
+                        ${goodLettersArray.includes(letter) ? (letter) : ('  ') }
                     </span>
                 `
             )
-            .join('')
+            .join(' ')
         }
     `;
-
+    
 };
 
 showWord();
