c92f8d9c4ab4c14663f9da426721963e7752f268
Etudiant O'clock <clarie.cambert@gmail.com>
2023-02-09 15:17:30 +0100

    projet fini, correction de bug en cours

--- js/script.js
+++ js/script.js
@@ -4,18 +4,17 @@ const btnReplay = document.getElementById('btn-message');
 const popup = document.getElementById('popup-content');
 const notification = document.getElementById('notification');
 const finalMessage = document.getElementById('final-message');
-
-
-const bodyMan = document.querySelectorAll('.body-man');
+const hangman = document.querySelectorAll('.hangman-part');
+const letterKeys = document.querySelectorAll('.key');
 
 const words = [
-    'telephone', 'chaussures', 'cheveux', 'universel', 'cinema', 'sport', 'chat', 'chien', 'marvel', 'voldemort', 'ordinateur', 'france', 'fenetre', 'tasse', 'constitution', 'code', 'bretagne', 'marseille', 'toilettes', 'chambre', 'restaurant', 'pomme', 'table', 'coussin', 'eponge', 'superman', 'paris', 'ampoule', 'croissant', 'oeufs', 'baguette', 'application', 'javascript'
-]
+    'TELEPHONE', 'CHAUSSURES', 'CHEVEUX', 'UNIVERSEL', 'CINEMA', 'SPORT', 'CHAT', 'CHIEN', 'MARVEL', 'VOLDEMORT', 'ORDINATEUR', 'FRANCE', 'FENETRE', 'TASSE', 'CONSTITUTION', 'CODE', 'BRETAGNE', 'MARSEILLE', 'TOILETTES', 'CHAMBRE', 'RESTAURANT', 'POMME', 'TABLE', 'COUSSIN', 'EPONGE', 'SUPERMAN', 'PARIS', 'AMPOULE', 'CROISSANT', 'OEUFS', 'BAGUETTE', 'APPLICATION', 'JAVASCRIPT'
+];
 
 let selectedWord = words[ Math.floor(Math.random() * words.length) ];
-console.log(selectedWord);
-const  goodLettersArray = [''];
-const  wrongLettersArray = [''];
+
+const  goodLettersArray = [];
+const  wrongLettersArray = [];
 
 // Afficher le mot caché, lettre par lettre
 function showWord() {
@@ -26,19 +25,20 @@ function showWord() {
             .map(
                 (letter) => `
                     <span id="single-letter">
-                        ${goodLettersArray.includes(letter) ? (letter) : ('  ') }
+                        ${goodLettersArray.includes(letter) ? (letter) : (' ') }
                     </span>
                 `
             )
             .join(' ')
         }
     `;
+
     const internalWord = wordEl.innerText.replace(/\n/g, '');
 
     console.log(internalWord);
     console.log(selectedWord);
 
-    if(internalWord === selectedWord) {
+    if(internalWord == selectedWord) {
         finalMessage.innerText = 'Bravo, tu as gagné!';
         popup.style.display = 'flex';
     } 
@@ -46,16 +46,29 @@ function showWord() {
 };
 
 // Mauvaises lettres 
-function updateWrongLetterElem(params) {
+function updateWrongLetterElem() {
     // Afficher les mauvaises lettres
     wrongLetters.innerHTML = `
         ${wrongLettersArray.map (
-            (letter) => `<span>${(letter)}</span>
-        `)}
+            (letter) => `<span> ${letter} </span>`
+        )}
     `
-    // Afficher le bonhomme
+    // Afficher le pendu
+    hangman.forEach((part, index) => {
+        const error = wrongLettersArray.length;
+
+        if (index < error) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    }) 
 
     // Verifier si perdant
+    if (wrongLettersArray.length === hangman.length) {
+        finalMessage.innerText = 'Haha, tu as perdu !';
+        popup.style.display = 'flex';
+    }
 }
 
 // Affichage notification
@@ -66,11 +79,14 @@ function showNotification() {
     }, 2000)
 }
 
+
 // Event Listener
 window.addEventListener('keydown', e => {
+
     if ( e.keyCode >= 65 && e.keyCode <= 90 ) {
+
         const letterTarget = e.key;
-        // console.log(letterTarget);
+
         if ( selectedWord.includes( letterTarget )) {
             if( !goodLettersArray.includes( letterTarget )) {
                 goodLettersArray.push( letterTarget );
@@ -80,13 +96,51 @@ window.addEventListener('keydown', e => {
             }
         } else {
             if( !wrongLettersArray.includes( letterTarget )) {
-                goodLettersArray.push( letterTarget );
+                wrongLettersArray.push( letterTarget );
                 updateWrongLetterElem();
             } else {
                 showNotification();
             }
         }
     }
+})
+
+for (const letterKey of letterKeys) {
+    
+    letterKey.addEventListener('click', e => {
+
+        const letterTarget = e.currentTarget.textContent;
+
+        if ( selectedWord.includes( letterTarget )) {
+            if( !goodLettersArray.includes( letterTarget )) {
+                goodLettersArray.push( letterTarget );
+                showWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if( !wrongLettersArray.includes( letterTarget )) {
+                wrongLettersArray.push( letterTarget );
+                updateWrongLetterElem();
+            } else {
+                showNotification();
+            }
+        }
+    })
+}
+
+btnReplay.addEventListener('click', () => {
+    goodLettersArray.splice(0);
+    wrongLettersArray.splice(0);
+
+    selectedWord = words[ Math.floor(Math.random() * words.length) ];
+    
+    showWord();
+
+    updateWrongLetterElem();
+
+    popup.style.display = 'none';
+    
 })
 
 showWord();
